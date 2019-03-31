09d3d3742f8ac1e4e3bf20dbcf09ec5ba27d6797
unknown <nathan.allani@laposte.net>
2019-03-31 16:20:27 +0100

    added first scripts

---
+++ Assets/Scripts/Player_gen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_gen : MonoBehaviour
+{
+    [SerializeField] Menu_inf menu_inf;
+    void Awake()
+    {
+        this.GetComponent<
+    }
+
+    void Update()
+    {
+        
+    }
+}
