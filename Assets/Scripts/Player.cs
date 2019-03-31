df595e604e338d56c3d88e6fe664a00adf3a5094
Hubert <hubertvromman1@Shamry.local>
2019-03-31 21:26:37 +0200

    Super perso ;)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] protected float max_speed = 20;
+    [SerializeField] protected float acceleration = 1;
+    protected float speed_multiplier = 1;
+    [SerializeField] protected Rigidbody2D rb;
+
     void Start()
     {
         
     }
 
+    void deplacement ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) && Mathf.Abs(rb.velocity.x) < max_speed)
+            rb.AddForce(new Vector2(1, 0));
+    }
+
     void Update()
     {
-       
+        deplacement();
     }
 }
---
+++ Assets/Scripts/Player_melee.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_melee : Player
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed_multiplier *= 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
